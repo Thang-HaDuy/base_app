eeeee4f6c39ed531bc89ed948b7c074ce53e0f55
Haduythang <haduythang33@gmail.com>
2023-09-02 15:37:04 +0700

    base code asp.net core 7.0

---
+++ Data/DataDbContext.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using App.Models;
+
+namespace App.Data
+{
+    public class DataDbContext : IdentityDbContext<AppUser>
+    {
+        
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+             foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        } 
+
+    }
+}
